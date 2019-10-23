edcbf7209cea8fa3a04190c42d358af659e33e76
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-23 10:06:10 +0800

    Make TodoList work properly(?)

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -15,10 +15,9 @@ class TodoList extends React.Component {
     render() {
         return(
             <div className="TodoList">
-                <input type="text" value={this.state.descInput} onChange={this.handleChange}></input>
+                <input type="text" onChange={this.handleChange}></input>
                 <button onClick={this.addTodo}>add</button>
                 <ul>{this.state.todoList}</ul>
-                {console.log('output' + this.state.descOutput)}
             </div>
         );
     }
@@ -26,7 +25,7 @@ class TodoList extends React.Component {
     addTodo = () => {
         if (this.state.descInput !== '') {
             this.setState({descOutput: this.state.descInput});
-            this.state.todoList.push(<Todo description={this.state.descOutput}/>);
+            this.state.todoList.push(<Todo description={this.state.descInput}/>);
         }
     }
 
